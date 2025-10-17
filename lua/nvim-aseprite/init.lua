257369e646893e65e3c17c2ddd15994dc11f0e0f
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-10-17 18:10:07 +0100

    remove debug print lines

---
+++ lua/nvim-aseprite/init.lua
@@ -0,0 +1,83 @@
+local nvim_aseprite = {}
+
+local uv = vim.uv
+local api = vim.api
+
+local function extend_lsp()
+	if not vim.lsp.config.lua_ls then
+		vim.notify("nvim_aseprite: extend_lsp: could not find lua_ls config", vim.log.levels.ERROR)
+		return
+	end
+
+	local settings = {
+		Lua = {
+			workspace = {
+				-- Without libary because vim.tbl_deep_extend overwrites lists
+				-- and user may have their own libraries configured. Rather
+				-- than force overwrite, we'll insert the libary below.
+			},
+		},
+	}
+	settings = vim.tbl_deep_extend("force", vim.lsp.config.lua_ls.settings or {}, settings)
+
+	-- Add libaries separately so that we can insert the Aseprite library
+	-- safely, without overwriting any pre-existing user libaries.
+	local libraries = {}
+	---@diagnostic disable-next-line: undefined-field
+	libraries = settings.Lua.workspace.library or {}
+	table.insert(libraries, vim.fn.globpath(vim.o.runtimepath, "aseprite-lualib"))
+	---@diagnostic disable-next-line: undefined-field
+	settings.Lua.workspace.library = libraries
+
+	-- Apply and restart the language server, because as of 2025-10-17, Neovim
+	-- LSP implementation does not yet support dynamic configuration updates.
+	vim.lsp.config("lua_ls", { settings = settings })
+	vim.lsp.enable("lua_ls", false)
+	vim.lsp.enable("lua_ls", true)
+end
+
+--- Returns true if dir is in an aseprite project, which is a project where
+--- there is a file named ".aseprite" within the project structure. Searches upwards
+--- until it finds the .aseprite file, or stopping once it hits a directory
+--- containing a .git directory, which is assumed to be project root.
+---
+--- @param dir string
+--- @return boolean
+nvim_aseprite.is_aseprite_project = function(dir)
+	while dir do
+		if uv.fs_stat(vim.fs.joinpath(dir, ".aseprite")) then
+			return true
+		end
+		if uv.fs_stat(vim.fs.joinpath(dir, ".git")) then
+			return false
+		end
+
+		local parent = uv.fs_realpath(vim.fs.joinpath(dir, ".."))
+		if not parent or parent == dir then
+			return false
+		end
+		dir = parent
+	end
+	return false
+end
+
+nvim_aseprite.setup = function()
+	-- if nvim_aseprite.is_aseprite_project(vim.fn.getcwd()) then
+	-- 	extend_lsp()
+	-- end
+
+	local group = api.nvim_create_augroup("nvim-aseprite", { clear = true })
+	api.nvim_create_autocmd("LspAttach", {
+		group = group,
+		once = true, -- Has to be one-shot, because otherwise the LSP restart causes an endless loop
+		callback = function(args)
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			if not client or client.name ~= "lua_ls" then
+				return
+			end
+			extend_lsp()
+		end,
+	})
+end
+
+return nvim_aseprite
